38e7b583da288f107149d09a1fcd37c3c47da9e5
ZhiweiChaichai <494682844@qq.com>
2020-10-28 19:15:52 +0800

    vuex-count计数器demo

--- src/components/Addition.vue
+++ src/components/Addition.vue
@@ -1,7 +1,13 @@
 <template>
   <div>
-    <h3>当前最新的count值为：</h3>
-    <button>+1</button>
+<!--    <h3>当前最新的count值为：{{$store.state.count}}</h3>-->
+    <!--    getters的第一种使用方式-->
+    <h3>{{$store.getters.showNum}}</h3>
+    <button @click="btnHandler1">+1</button>
+    <button @click="btnHandler2">+N</button>
+    <!-- 异步加一：延时一秒再加一-->
+    <button @click="btnHandler3">+1 Async</button>
+    <button @click="btnHandler4">+N Async</button>
   </div>
 </template>
 
@@ -9,6 +15,25 @@
 export default {
   data () {
     return {}
+  },
+  methods: {
+    btnHandler1 () {
+      // this.$store.state.count++  //这种做法直接修改store里的数据，不便于后期的维护，不可以使用
+      this.$store.commit('add')
+    },
+    btnHandler2 () {
+      // commit的作用就是调用某个mutation函数
+      this.$store.commit('addN', 3)
+    },
+    // 异步的让count+1
+    btnHandler3 () {
+      // 触发action的第一种方式
+      // 这里的dispatch函数，专门用来触发某个mutation
+      this.$store.dispatch('addAsync')
+    },
+    btnHandler4 () {
+      this.$store.dispatch('addNAsync', 3)
+    }
   }
 }
 </script>
--- src/store/index.js
+++ src/store/index.js
@@ -5,10 +5,67 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    count: 0
   },
+  // 只有mutation才有权力去变更store里的数据，不可以在外部组件的其他函数里直接对store里的数据进行操作
   mutations: {
+    add (state) {
+      // 变更状态
+      // 不要在mutations函数中，执行异步操作
+      // setTimeout(() => {
+      //   state.count++
+      // }, 1000)
+      state.count++
+    },
+    // 传递参数，step是传的值
+    addN (state, step) {
+      // 变更状态
+      state.count += step
+    },
+    sub (state) {
+      state.count--
+    },
+    subN (state, step) {
+      // 变更状态
+      state.count -= step
+    }
   },
+  // 用于处理异步任务
+  // action还是要通过出发Mutation的方式间接变更数据
   actions: {
+    addAsync (context) {
+      setTimeout(() => {
+        // 在actions中，不能直接修改state中的数据
+        // 必须通过context.commit() 触发某个mutation才行
+        context.commit('add')
+      }, 1000)
+    },
+    addNAsync (context, step) {
+      setTimeout(() => {
+        // 在actions中，不能直接修改state中的数据
+        // 必须通过context.commit() 触发某个mutation才行
+        context.commit('addN', step)
+      }, 1000)
+    },
+    subAsync (context) {
+      setTimeout(() => {
+        // 在actions中，不能直接修改state中的数据
+        // 必须通过context.commit() 触发某个mutation才行
+        context.commit('sub')
+      }, 1000)
+    },
+    subNAsync (context, step) {
+      setTimeout(() => {
+        // 在actions中，不能直接修改state中的数据
+        // 必须通过context.commit() 触发某个mutation才行
+        context.commit('subN', step)
+      }, 1000)
+    }
+  },
+  getters: {
+    showNum (state) {
+      return '当前最新的数量是【' + state.count + '】'
+    }
   },
   modules: {
   }
